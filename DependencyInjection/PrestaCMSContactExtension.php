2f1f7393e9d6ed33efe392e2a429b46ff9549c57
Nicolas Bastien <nbastien@prestaconcept.net>
2013-10-09 17:29:59 +0200

    Bundle set-up

---
+++ DependencyInjection/PrestaCMSContactExtension.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the PrestaCMSContactBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSContactBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ *
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaCMSContactExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
---
+++ PrestaCMSContactBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the PrestaCMSContactBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSContactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaCMSContactBundle extends Bundle
+{
+}
