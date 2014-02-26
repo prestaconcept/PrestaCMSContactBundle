bc206dbbbe8fcf665495378474e49a9cdc731be6
Nicolas Bastien <nbastien.pro@gmail.com>
2014-02-26 14:36:49 +0100

    Fix : Unused use statement should be avoided

--- Admin/MessageAdmin.php
+++ Admin/MessageAdmin.php
@@ -12,7 +12,6 @@ namespace Presta\CMSContactBundle\Admin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Admin\Admin;
-use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
 
