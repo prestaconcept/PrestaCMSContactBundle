12a8b761c4784a6f1c0d844e50a2865d38426ef7
Nicolas Bastien <nbastien@prestaconcept.net>
2014-03-05 10:30:15 +0100

    Correction ORM mapping

--- PrestaCMSContactBundle.php
+++ PrestaCMSContactBundle.php
@@ -33,17 +33,43 @@ class PrestaCMSContactBundle extends Bundle
      */
     private function addRegisterMappingsPass(ContainerBuilder $container)
     {
-        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createXmlMappingDriver(
-                    array(
-                        realpath($this->getPath()  . '/Resources/config/doctrine-model') => 'Presta\CMSContactBundle\Model',
-                        realpath($this->getPath()  . '/Resources/config/doctrine-orm')   => 'Presta\CMSContactBundle\Doctrine\Orm'
-                    ),
-                    array('presta_cms_contact.persistence.orm.manager_name'),
-                    'presta_cms_contact.backend_type_orm'
-                )
-            );
+        $doctrineOrmCompiler = $this->findDoctrineOrmCompiler();
+        if (!$doctrineOrmCompiler) {
+            return;
         }
+
+        $container->addCompilerPass(
+            $doctrineOrmCompiler::createXmlMappingDriver(
+                array(
+                    realpath($this->getPath()  . '/Resources/config/doctrine-model') => 'Presta\CMSContactBundle\Model',
+                    realpath($this->getPath()  . '/Resources/config/doctrine-orm')   => 'Presta\CMSContactBundle\Doctrine\Orm'
+                ),
+                array('presta_cms_contact.persistence.orm.manager_name'),
+                'presta_cms_contact.backend_type_orm'
+            )
+        );
+    }
+
+    /**
+     * Looks for a mapping compiler pass. If available, use the one from
+     * DoctrineBundle (available only since DoctrineBundle 2.4 and Symfony 2.3)
+     * Otherwise use the standalone one from CmfCoreBundle.
+     *
+     * @return boolean|string the compiler pass to use or false if no suitable
+     *      one was found
+     */
+    private function findDoctrineOrmCompiler()
+    {
+        if (class_exists('Symfony\Bridge\Doctrine\DependencyInjection\CompilerPass\RegisterMappingsPass')
+            && class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')
+        ) {
+            return 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+        }
+
+        if (class_exists('Symfony\Cmf\Bundle\CoreBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
+            return 'Symfony\Cmf\Bundle\CoreBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+        }
+
+        return false;
     }
 }
